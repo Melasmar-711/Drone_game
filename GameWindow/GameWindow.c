aec4c9143cbc35c1c967d18ff41b2a04db9b1ae1
Melasmar-711 <mahmoudelasmar2000@outlook.com>
2024-12-02 22:52:57 +0100

    added dependencies to the CMakeLists and improved the GameWidnow which now shows the score

--- GameWindow/GameWindow.c
+++ GameWindow/GameWindow.c
@@ -6,11 +6,15 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/select.h>
+#include <math.h>
+
 
 
 #define MAX_OBSTACLES 10
 #define MAX_TARGETS 10
-#define MAX_X 50
+
+#define Target_catched 200
+#define MAX_X 100
 #define MAX_Y 20
 
 typedef struct {
@@ -48,7 +52,7 @@ static inline int create_and_open_fifo(const char *fifo_name, int flags) {
 // Function prototypes
 void init_ncurses();
 void draw_borders();
-void draw_simulation(ServerState *prev_state, ServerState *current_state);
+void draw_simulation(ServerState *prev_state, ServerState *current_state,int *flags);
 
 int main() {
     
@@ -57,11 +61,10 @@ int main() {
     struct timeval timeout = {0, 0};
 
 
-    int fd_server_to_GameWindow = create_and_open_fifo("/tmp/server_to_GameWindow", O_RDONLY | O_NONBLOCK);
-    if (fd_server_to_GameWindow < 0) {
-        perror("Failed to open pipe");
-        return 1;
-    }
+    int fd_server_to_GameWindow = create_and_open_fifo("/tmp/server_to_GameWindow", O_RDONLY );
+
+
+
 
 
 
@@ -82,10 +85,14 @@ int main() {
     };
 
 
+    int target_active_flags[state.num_obstacles];
+    memset(target_active_flags, 0, sizeof(target_active_flags)); // Set all bytes of arr to 0
+
+
     ServerState prev_state={0};
     init_ncurses();
     draw_borders();
-    draw_simulation(&prev_state,&state);
+    draw_simulation(&prev_state,&state,target_active_flags);
 
     refresh();
 
@@ -93,41 +100,52 @@ int main() {
     while (1) {
 
 
-        FD_ZERO(&read_fds);
-        FD_SET(fd_server_to_GameWindow, &read_fds);
+        //FD_ZERO(&read_fds);
+        //FD_SET(fd_server_to_GameWindow, &read_fds);
 
-        int activity = select(fd_server_to_GameWindow + 1, &read_fds, NULL, NULL, &timeout);
+        //int activity = select(fd_server_to_GameWindow + 1, &read_fds, NULL, NULL, &timeout);
 
-        if (activity>0){
+        //if (activity>0){
 
 
-            ssize_t bytes_read = read(fd_server_to_GameWindow, &state, sizeof(ServerState));
-        }
+        ssize_t bytes_read = read(fd_server_to_GameWindow, &state, sizeof(ServerState));
+        //}
 
         
-        draw_simulation(&prev_state,&state);
+        draw_simulation(&prev_state,&state,target_active_flags);
+
         refresh();
-        prev_state=state;
 
-        
-        
-        
+        prev_state=state; 
         usleep(DELAY);
     }
+
+
 
     endwin();
     close(fd_server_to_GameWindow);
     return 0;
 }
 
+
+
 void init_ncurses() {
     initscr();
     noecho();
     curs_set(FALSE);
+    start_color();
+    init_pair(1, COLOR_BLUE, COLOR_BLACK);  // Drone
+    init_pair(2, COLOR_GREEN, COLOR_BLACK); // Targets
+    init_pair(3, COLOR_RED, COLOR_BLACK);   // Obstacles
 }
 
+
+
+
 void draw_borders() {
+
     // Draw the static simulation boundary
+    attron(COLOR_PAIR(3));
     for (int i = 0; i < MAX_X; i++) {
         mvprintw(0, i, "-");
         mvprintw(MAX_Y, i, "-");
@@ -139,51 +157,112 @@ void draw_borders() {
     }
 
     refresh(); // Refresh to display the borders
+    attroff(COLOR_PAIR(3)); // Stop using the obstacle color
 }
 
-void draw_simulation(ServerState *prev_state, ServerState *current_state) {
-    // Handle the drone position
-    if (prev_state->drone_x != current_state->drone_x || prev_state->drone_y != current_state->drone_y) {
-        // Erase old drone position
 
+
+void draw_simulation(ServerState *prev_state, ServerState *current_state,int *flags) {
+    // Handle the drone position
+    if (prev_state->drone_x != current_state->drone_x || prev_state->drone_y != current_state->drone_y) 
+    {
+        // Erase old drone position
+        attron(COLOR_PAIR(1));
         mvprintw((int) prev_state->drone_y, (int)prev_state->drone_x, " ");
         // Draw new drone position
-        mvprintw((int)current_state->drone_y, (int)current_state->drone_x, "D");
+        mvprintw((int)current_state->drone_y, (int)current_state->drone_x, "+");
+
+        attroff(COLOR_PAIR(1));
     }
 
     // Handle obstacles
-    for (int i = 0; i < MAX_OBSTACLES; i++) {
-        if (i >= prev_state->num_obstacles && i < current_state->num_obstacles) {
+    for (int i = 0; i < MAX_OBSTACLES; i++) 
+    {
+
+        attron(COLOR_PAIR(3));
+        
+        if (i >= prev_state->num_obstacles && i < current_state->num_obstacles)
+        {
             // New obstacle added
             mvprintw(current_state->obstacles[i][1], current_state->obstacles[i][0], "O");
-        } else if (i < prev_state->num_obstacles && i >= current_state->num_obstacles) {
+        } 
+        else if (i < prev_state->num_obstacles && i >= current_state->num_obstacles) 
+        {
             // Obstacle removed
             mvprintw(prev_state->obstacles[i][1], prev_state->obstacles[i][0], " ");
-        } else if (i < prev_state->num_obstacles && i < current_state->num_obstacles) {
+        } 
+        else if (i < prev_state->num_obstacles && i < current_state->num_obstacles) 
+        {
             // Check if an obstacle moved
             if (prev_state->obstacles[i][0] != current_state->obstacles[i][0] ||
-                prev_state->obstacles[i][1] != current_state->obstacles[i][1]) {
+                prev_state->obstacles[i][1] != current_state->obstacles[i][1]) 
+            {
                 mvprintw(prev_state->obstacles[i][1], prev_state->obstacles[i][0], " "); // Erase old position
                 mvprintw(current_state->obstacles[i][1], current_state->obstacles[i][0], "O"); // Draw new position
             }
         }
+
+        attroff(COLOR_PAIR(3));
     }
 
+
+
     // Handle targets
-    for (int i = 0; i < MAX_TARGETS; i++) {
-        if (i >= prev_state->num_targets && i < current_state->num_targets) {
+    for (int i = 0; i < MAX_TARGETS; i++) 
+    {
+
+
+        static int score=0;
+
+        int dx = current_state->drone_x - current_state->targets[i][0];
+        int dy = current_state->drone_y - current_state->targets[i][1];
+        double distance = sqrt(dx * dx + dy * dy);
+
+        if (distance<0.2 && flags[i]!=1 )
+        {
+            flags[i]=1;  // this means it's taken now
+            score++;
+
+        }
+
+        attron(COLOR_PAIR(2));
+
+        if (i >= prev_state->num_targets && i < current_state->num_targets   && flags[i]==0) 
+        {
             // New target added
             mvprintw(current_state->targets[i][1], current_state->targets[i][0], "T");
-        } else if (i < prev_state->num_targets && i >= current_state->num_targets) {
+        } 
+        
+        else if (((i < prev_state->num_targets && i >= current_state->num_targets ) ) || flags[i]==1 ) 
+        {
             // Target removed
             mvprintw(prev_state->targets[i][1], prev_state->targets[i][0], " ");
-        } else if (i < prev_state->num_targets && i < current_state->num_targets) {
-            // Check if a target moved
-            if (prev_state->targets[i][0] != current_state->targets[i][0] ||
-                prev_state->targets[i][1] != current_state->targets[i][1]) {
-                mvprintw(prev_state->targets[i][1], prev_state->targets[i][0], " "); // Erase old position
-                mvprintw(current_state->targets[i][1], current_state->targets[i][0], "T"); // Draw new position
-            }
+
         }
+
+        else if (i < prev_state->num_targets && i < current_state->num_targets) 
+        {
+            // Check if a target moved
+            if (prev_state->targets[i][0] != current_state->targets[i][0] || prev_state->targets[i][1] != current_state->targets[i][1])      
+            {
+
+
+                mvprintw(prev_state->targets[i][1], prev_state->targets[i][0], " "); // Erase old position
+                
+                mvprintw(current_state->targets[i][1], current_state->targets[i][0], "T"); // Draw new position
+
+
+            }
+
+
+        }
+
+        mvprintw(35,40,"score : "); // the score
+
+        mvprintw(35,48,"%d", score); // the score
+
+
+        attroff(COLOR_PAIR(2));
+
     }
 }
